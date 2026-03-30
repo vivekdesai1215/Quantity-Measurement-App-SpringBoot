6fd8a0db9a5f0042b4ff7644cf7f436477669e63
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-03-30 11:45:06 +0530

    Added test folder

---
+++ src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityMeasurementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
