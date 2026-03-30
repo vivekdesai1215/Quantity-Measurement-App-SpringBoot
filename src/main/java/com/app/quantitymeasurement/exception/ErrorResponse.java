91839bcfd3fb12faea29a0cb52223b64420a46df
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-03-30 11:39:40 +0530

    Custom response Class

---
+++ src/main/java/com/app/quantitymeasurement/exception/ErrorResponse.java
@@ -0,0 +1,57 @@
+package com.app.quantitymeasurement.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+	public LocalDateTime timeStamp;
+	public int status;
+	public String error;
+	public String message;
+	public String path;
+			
+	public ErrorResponse(LocalDateTime timeStamp, int status, String error, String message, String path) {
+		super();
+		this.timeStamp = timeStamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+	
+	public ErrorResponse() {
+		
+	}
+	
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(LocalDateTime timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
