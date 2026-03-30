c074ea387af2da97c31c9010ec41d02839e9f0cf
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-03-30 11:40:13 +0530

    Created custom exception

---
+++ src/main/java/com/app/quantitymeasurement/exception/QuantityMeasurementException.java
@@ -0,0 +1,7 @@
+package com.app.quantitymeasurement.exception;
+
+public class QuantityMeasurementException extends RuntimeException {
+    public QuantityMeasurementException(String message) {
+        super(message);
+    }
+}
