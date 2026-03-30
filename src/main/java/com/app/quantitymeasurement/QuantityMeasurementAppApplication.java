76fa9359b8805c751db5769b30941da29636b6ea
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-03-30 10:52:44 +0530

    Application Starting point

---
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.quantitymeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityMeasurementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityMeasurementAppApplication.class, args);
+	}
+
+}
